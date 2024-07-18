041dae1db0a9e64ff61c46b078c1bc033b06b1ce
陈碧滔 <bitao.chen@qizhidao.com>
2024-07-18 11:27:04 +0800

    build: 指定playground/nuxt-project应用的兼容日期

--- playground/nuxt-project/nuxt.config.ts
+++ playground/nuxt-project/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  /**
+   * 指定应用的兼容日期。
+   *
+   * 这用于控制 Nitro、Nuxt Image 和其他模块中预设的行为，这些预设可能会在没有主要版本颠簸的情况下改变行为。 我们计划在未来改进有关此功能的工具。
+   * @see https://nuxt.com/docs/api/nuxt-config#compatibilitydate
+   */
+  compatibilityDate: '2024-07-18',
 })
